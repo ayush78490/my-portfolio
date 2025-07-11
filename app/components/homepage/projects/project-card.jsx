51d7aed82083d9e673524621c16691c3e79b04c7
unknown <tarunsingh78490@gmail.com>
2025-07-11 09:06:26 +0530

    updated the projects

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -61,6 +61,20 @@ function ProjectCard({ project }) {
             <span className="text-cyan-400">{' ' + project.description}</span>
             <span className="text-gray-400">,</span>
           </div>
+          {project.livePreview && (
+            <div className="ml-4 lg:ml-8 mr-2">
+              <span className="text-white">livePreview:</span>
+              <a 
+                href={project.livePreview} 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="text-blue-400 hover:text-blue-300 hover:underline ml-2"
+              >
+                {project.livePreview}
+              </a>
+              <span className="text-gray-400">,</span>
+            </div>
+          )}
           <div><span className="text-gray-400">{`};`}</span></div>
         </code>
       </div>
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: '/Profile.png',
   designation: "Software Developer",
   description: "My name is Ayush Raj. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'tarunsingh78490@gmail.com',
+  email: 'ayushraj784900@gmail.com',
   phone: '+919570490054',
   address: 'Mohali, Punjab, India - 140103 ',
   github: 'https://github.com/ayush78490',
